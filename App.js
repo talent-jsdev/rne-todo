ff6dfe239698790706a10c1c8b24cb5f84e8dadf
unknown <webdevelopercrane@gmail.com>
2020-08-18 17:38:45 +0700

    Add Button

--- App.js
+++ App.js
@@ -1,35 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, Button, SafeAreaView } from "react-native";
 
 export default function App() {
-  let x = 1;
   console.log("executed");
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      {/* <TouchableWithoutFeedback> */}
-      <TouchableOpacity>
-        <Image
-          // blurRadius={10}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableOpacity>
-      {/* </TouchableWithoutFeedback> */}
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => alert("ButtonTapped")}
+      />
     </SafeAreaView>
   );
 }
