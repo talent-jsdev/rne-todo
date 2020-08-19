36224a1287a076075c32a3faed18839fc8793328
Phantom <phantom@Phantoms-Mac.local>
2020-08-19 03:09:00 -0700

    Add Alert.prompt in only iOS

--- App.js
+++ App.js
@@ -9,10 +9,7 @@ export default function App() {
         color="orange"
         title="Click Me"
         onPress={() =>
-          Alert.alert("My title", "My message", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
+          Alert.prompt("My title", "My message", text => console.log(text))
         }
       />
     </SafeAreaView>
