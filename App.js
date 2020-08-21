78095b5726eb6703ae0a53d545dbdb135128a25c
Phantom <phantom@Phantoms-Mac.local>
2020-08-21 01:56:02 -0700

    Create WelcomeScreen

--- App.js
+++ App.js
@@ -1,24 +1,6 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, StatusBar, Platform, View } from "react-native";
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: "100%",
-        height: landscape ? "100%" : "30%",
-      }}></View>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "ios" ? StatusBar.currentHeight : 0,
-  },
-});
